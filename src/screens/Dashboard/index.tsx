e723dac700eb9f126c8285810e53683207ff6715
rhaynon <rhaynonsoares@gmail.com>
2024-04-08 11:37:23 -0300

    Base comit

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Container, Title} from './styles' 
+
+
+
+export function Dashboard(){
+    return (
+        <Container>
+            <Title>Dashboard</Title>
+        </Container>
+    )
+}
+
+
